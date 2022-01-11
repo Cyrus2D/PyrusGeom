b8d936212c85a29cefce937d9e537a27178c2472
mroa4 <omidamini360@gmail.com>
2022-01-12 01:45:04 +0330

    clean update angle_deg - test add for angle_deg - update .gitignore

--- PyrusGeom/angle_deg.py
+++ PyrusGeom/angle_deg.py
@@ -5,7 +5,24 @@ import math
 
 
 class AngleDeg:
+    """ handling degrees in SS2D
+    Attributes:
+        degree: a float for angles
+    """
     def __init__(self, *args) -> None:
+        """This is the class init function and normalizes the input degree.
+        
+        Defualt:
+            bulid a AngleDeg with value of 0 degree
+        Args:
+            *args:
+                none: for default angle
+                one: 
+                    AngleDeg: create a new AngleDeg with normalized input AngleDeg
+                    Degree: create an AngleDeg with normalized degree
+        Raises:
+            Exception: Input must be an AngleDeg or a Degree
+        """
         if len(args) == 0:
             self._degree = 0
         elif len(args) == 1:
@@ -14,19 +31,51 @@ class AngleDeg:
             else:
                 self._degree = args[0]
         else:
-            raise Exception('The input should be an AngleDeg or a degree')
+            raise Exception('The input should be an AngleDeg or a Degree')
         self.normal()
 
+
     def degree(self) -> float:
+        """get the degree in a new AngleDeg
+
+        Returns:
+            float: a new AngleDeg degree with the old AngleDeg's degree
+        """
+        return AngleDeg(self._degree)._degree
+    
+    def degree_(self) -> float:
+        """get the orginal degree
+
+        Returns:
+            float: AngleDeg's degree
+        """
         return self._degree
 
     def set_degree(self, degree: float) -> None:
+        """set a new degree for AngleDeg and normalizes it. 
+
+        Args:
+            degree (float): new degree to set
+        """
         self._degree = degree
+        self.normal()
 
     def set_angle(self, angle: AngleDeg) -> None:
+        """set a new degree for AngleDeg from another AngleDeg
+
+        Args:
+            angle (AngleDeg): an AngleDeg to copy the degree
+        """
         self._degree = angle.degree()
 
     def normal(self) -> None:
+        """normalizing the degree
+        
+        if the angle is below -360 degree or above 360 degree,
+        it will calculating module of given degree by 360,
+        and cap the angle between -180 degree and 180 degree.
+
+        """
         if self._degree < -360.0 or 360.0 < self._degree:
             self._degree = math.fmod(self._degree, 360.0)
 
@@ -37,6 +86,15 @@ class AngleDeg:
             self._degree -= 360.0
 
     def is_within(self, left: Union[AngleDeg, float, int], right: Union[AngleDeg, float, int]) -> bool:
+        """check if this AngleDeg is within [left, right] (turn clockwise)
+
+        Args:
+            left (Union[AngleDeg, float, int]): left angle
+            right (Union[AngleDeg, float, int]): right angle
+
+        Returns:
+            bool: true if this is within [left, right]. else false
+        """
         if not isinstance(left, AngleDeg):
             left = AngleDeg(left)
         if not isinstance(right, AngleDeg):
@@ -50,54 +108,122 @@ class AngleDeg:
         return False
 
     def abs(self) -> float:
+        """get absolute value of this AngleDeg
+
+        Returns:
+            float: absolute value of degree()
+        """
         return math.fabs(self.degree())
 
     def radian(self) -> float:
+        """get RADIAN value.
+
+        Returns:
+            float:  radian value of degree()
+        """
         return self.degree() * DEG2RAD
 
-    def reverse(self) -> AngleDeg:
+    def copy(self) -> AngleDeg:
+        """ copy the AngleDeg
+
+        Returns:
+            AngleDeg: a new AngleDeg with same values
+        """
+        return AngleDeg(self._degree)
+
+    def reverse(self) -> None:
+        """reverse this AngleDeg - 180 degrees opposite
+        """
         if self._degree >= 0:
             self._degree = -(180 - self._degree)
         else:
             self._degree = 180 + self._degree
-        return self
 
     def reverse_angle(self) -> AngleDeg:
+        """make a reversed AngleDeg - 180 degrees opposite
+
+        Returns:
+            AngleDeg: a new AngleDeg with reverse values
+        """
         angle = AngleDeg(self.degree())
         angle.reverse()
         return angle
 
+    def get_normalized(self) -> float:
+        """get the Normalized degree betwhen 0 and 1
+
+        Returns:
+            float: Normalized degree betwhen 0 and 1
+        """
+        return (self.degree() + 180) / 360
+
     def is_left_of(self, angle: Union[AngleDeg, float, int]) -> bool:
+        """check if this AngleDeg is left of [angle]
+
+        Args:
+            angle (Union[AngleDeg, float, int]): angle to check
+
+        Returns:
+            bool: true if this AngleDeg is left of [angle]. else false
+        """
         if not isinstance(angle, AngleDeg):
             angle = AngleDeg(angle)
         diff = angle.degree() - self.degree()
         return (0.0 < diff < 180.0) or diff < -180.0
 
     def is_right_of(self, angle: Union[AngleDeg, float, int]) -> bool:
+        """check if this AngleDeg is right of [angle]
+
+        Args:
+            angle (Union[AngleDeg, float, int]): angle to check
+
+        Returns:
+            bool: true if this AngleDeg is right of [angle]. else false
+        """
         if not isinstance(angle, AngleDeg):
             angle = AngleDeg(angle)
         diff = self.degree() - angle.degree()
         return (0.0 < diff < 180.0) or diff < -180.0
 
     def is_left_equal_of(self, angle: Union[AngleDeg, float, int]) -> bool:
+        """check if this AngleDeg is left or equal of [angle]
+
+        Args:
+            angle (Union[AngleDeg, float, int]): angle to check
+
+        Returns:
+            bool: true if this AngleDeg is left of [angle]. else false
+        """
         if not isinstance(angle, AngleDeg):
             angle = AngleDeg(angle)
         diff = angle.degree() - self._degree
         return 0.0 <= diff < 180.0 or diff < -180.0
 
     def is_right_equal_of(self, angle: Union[AngleDeg, float, int]) -> bool:
+        """check if this AngleDeg is right or equal of [angle]
+
+        Args:
+            angle (Union[AngleDeg, float, int]): angle to check
+
+        Returns:
+            bool: true if this AngleDeg is right of [angle]. else false
+        """
         if not isinstance(angle, AngleDeg):
             angle = AngleDeg(angle)
         diff = self.degree() - angle.degree()
         return (0.0 <= diff < 180.0) or diff < -180.0
 
-    def copy(self) -> AngleDeg:
-        return AngleDeg(self._degree)
 
-    def get_normalized(self) -> float:
-        return (self.degree() + 180) / 360
 
     def __iadd__(self, other: Union[AngleDeg, float, int]) -> AngleDeg:
+        """operator +=
+
+        Args:
+            other (Union[AngleDeg, float, int]): angle added value
+
+        Returns:
+            AngleDeg: this AngleDeg
+        """
         if isinstance(other, AngleDeg):
             other = other.degree()
         self._degree += other
@@ -105,6 +231,14 @@ class AngleDeg:
         return self
 
     def __isub__(self, other: Union[AngleDeg, float, int]) -> AngleDeg:
+        """operator -=
+
+        Args:
+            other (Union[AngleDeg, float, int]): angle subtract argument
+
+        Returns:
+            AngleDeg: this AngleDeg
+        """
         if isinstance(other, AngleDeg):
             other = other.degree()
         self._degree -= other
@@ -112,16 +246,41 @@ class AngleDeg:
         return self
 
     def __imul__(self, other: Union[float, int]) -> AngleDeg:
+        """operator *=
+
+        Args:
+            other (Union[float, int]): scalar multiply argument
+
+        Returns:
+            AngleDeg: this AngleDeg
+        """
         self._degree *= other
         self.normal()
         return self
 
     def __idiv__(self, other: Union[float, int]) -> AngleDeg:
+        """operator /=
+
+        Args:
+            other (Union[float, int]): scalar division argument
+
+        Returns:
+            AngleDeg: this AngleDeg
+        """        
         self._degree /= other
         self.normal()
         return self
 
     def __add__(self, other: Union[AngleDeg, float, int]) -> AngleDeg:
+        """operator add for AngleDeg
+
+        Args:
+            other (Union[AngleDeg, float, int]): right hand side argument 
+
+        Returns:
+            AngleDeg: a new Angledeg with value of 
+            sum of left hand side argument and right hand side argument
+        """        
         if type(other) == AngleDeg:
             new_angle_deg = AngleDeg(self._degree + other.degree())
         else:
@@ -129,76 +288,195 @@ class AngleDeg:
         return new_angle_deg
 
     def __sub__(self, other: Union[AngleDeg, float, int]) -> AngleDeg:
+        """operator sub for AngleDeg
+
+        Args:
+            other (Union[AngleDeg, float, int]): right hand side argument 
+
+        Returns:
+            AngleDeg: a new Angledeg with value of 
+            subtraction of right hand side argument from left hand side argument
+        """    
         if type(other) == AngleDeg:
             return AngleDeg(self._degree - other._degree)
         else:
             return AngleDeg(self._degree - other)
 
     def __mul__(self, other: Union[float, int]) -> AngleDeg:
+        """operator mul for AngleDeg
+
+        Args:
+            other (Union[float, int]): scalar multiply argument
+
+        Returns:
+            AngleDeg: a new AngleDeg with the multiply of this AngleDeg and 
+            scalar multiply argument
+        """
         new_angle_deg = AngleDeg(self._degree * other)
         return new_angle_deg
 
     def __floordiv__(self, other: Union[float, int]) -> AngleDeg:
+        """operator floordiv for AngleDeg
+
+        Args:
+            other (Union[float, int]): scalar division argument
+
+        Returns:
+            AngleDeg: a new AngleDeg with the floor division of this AngleDeg 
+            on scalar division argument
+        """
         new_angle_deg = AngleDeg(self._degree / other)
         return new_angle_deg
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """represent AngleDeg as a string
+
+        Returns:
+            str: AngleDeg's Degree as strting
+        """
         return str(self.degree())
 
-    def __float__(self):
+    def __float__(self) -> float:
+        """represent AngleDeg as a float
+
+        Returns:
+            float: AngleDeg's Degree as float
+        """
         return float(self.degree())
 
     def __neg__(self) -> AngleDeg:
+        """ negation operator
+
+        Returns:
+            AngleDeg: new AngleDeg with negation
+        """
         new_angle_deg = AngleDeg(-self._degree)
         return new_angle_deg
 
-    def __eq__(self, other):
-        if type(other) == AngleDeg:
-            self._degree = other.degree()
-        else:
-            self._degree = other
-            self.normal()
+    def __eq__(self, other:Union[AngleDeg,float, int]) -> bool:
+        """operator == for AngleDeg
+
+        Args:
+            other (Union[AngleDeg,float, int]): right hand side argument
+        Returns:
+            bool: true if equal or diffrence is less than EPSILON. else false
+        """
+        if isinstance(other, AngleDeg):
+            other = other.degree()
+        return math.fabs(self._degree - other) < EPSILON
+        
 
     def cos(self) -> float:
+        """calculate cosine
+
+        Returns:
+            float: cosine value
+        """
         return math.cos(self._degree * DEG2RAD)
 
     def sin(self) -> float:
+        """calculate sine
+
+        Returns:
+            float: sine value
+        """
         return math.sin(self._degree * DEG2RAD)
 
     def tan(self) -> float:
+        """calculate tarngetn
+
+        Returns:
+            float: tarngetn value
+        """        
         return math.tan(self._degree * DEG2RAD)
 
     @staticmethod
     def rad2deg(rad: float) -> float:
+        """static utility. convert radian to degree
+
+        Args:
+            deg (float): radian value
+
+        Returns:
+            float: degree value
+        """
         return rad * RAD2DEG
 
     @staticmethod
     def deg2rad(deg: float) -> float:
+        """static utility. convert degree to radian
+
+        Args:
+            deg (float): degree value
+
+        Returns:
+            float: radian value
+        """
         return deg * DEG2RAD
 
     @staticmethod
     def cos_deg(deg: float) -> float:
+        """static utility. calculate cosine value for degree angle
+
+        Args:
+            deg (float): degree value
+
+        Returns:
+            float: cosine value
+        """
         return math.cos(AngleDeg.deg2rad(deg))
 
     @staticmethod
     def sin_deg(deg: float) -> float:
+        """static utility. calculate sine value for degree angle
+
+        Args:
+            deg (float): degree value
+
+        Returns:
+            float: sine value
+        """
         return math.sin(AngleDeg.deg2rad(deg))
 
     @staticmethod
     def tan_deg(deg: float) -> float:
+        """static utility. calculate tangent value for degree angle
+
+        Args:
+            deg (float): degree value
+
+        Returns:
+            float: tangent value
+        """
         return math.tan(AngleDeg.deg2rad(deg))
 
     @staticmethod
     def acos_deg(cosine: float) -> float:
+        """static utility. calculate arc cosine value
+
+        Args:
+            sine (float): cosine value
+
+        Returns:
+            float: arc cosine value, that is degree type.
+        """
         if cosine >= 1.0:
             return 0.0
         elif cosine <= -1.0:
             return 180.0
         else:
-            AngleDeg.rad2deg(math.acos(cosine))
+            return AngleDeg.rad2deg(math.acos(cosine))
 
     @staticmethod
     def asin_deg(sine: float) -> float:
+        """static utility. calculate arc sine value
+
+        Args:
+            sine (float): sine value
+
+        Returns:
+            float: arc sine value, that is degree type.
+        """
         if sine >= 1.0:
             return 90.0
         elif sine <= -1.0:
@@ -208,17 +486,45 @@ class AngleDeg:
 
     @staticmethod
     def atan_deg(tangent: float) -> float:
+        """static utility. calculate arc tangent value
+
+        Args:
+            tangent (float): tangent value
+
+        Returns:
+            float: arc tangent value, that is degree.
+        """
         return AngleDeg.rad2deg(math.atan(tangent))
 
     @staticmethod
     def atan2_deg(y: float, x: float) -> float:
+        """static utility. calculate arc tangent value from XY
+
+        Args:
+            y (float): coordinate Y
+            x (float): coordinate X
+
+        Returns:
+            float: arc tangent value.
+        """
         if math.fabs(x) < EPSILON and math.fabs(y) < EPSILON:
             return 0.0
         else:
             return AngleDeg.rad2deg(math.atan2(y, x))
 
     @staticmethod
-    def bisect(left: Union[int, float, AngleDeg], right: Union[int, float, AngleDeg]) -> Union[AngleDeg, float]:
+    def bisect(left: Union[int, float, AngleDeg], 
+    right: Union[int, float, AngleDeg]) -> Union[AngleDeg, float]:
+        """static utility that returns bisect angle of [left, right]
+        this method can take obtuse angle
+
+        Args:
+            left (Union[int, float, AngleDeg]): left start angle
+            right (Union[int, float, AngleDeg]): right end angle
+
+        Returns:
+            Union[AngleDeg, float]: bisect angle
+        """
         result = AngleDeg(left)
         rel = AngleDeg(right - left)
         diff = result.degree() - AngleDeg(right).degree()
--- PyrusGeomTest/test_angle_deg.py
+++ PyrusGeomTest/test_angle_deg.py
@@ -35,21 +35,18 @@ class TestAngleDeg(TestCase):
         self.assertTrue(c.is_within(a, b))
         self.assertFalse(c.is_within(b, a))
 
-    def test_is_left_equal_of(self):
-        a = AngleDeg(20)
-        b = AngleDeg(30)
-        self.fail()
-
     def test_abs(self):
         a = AngleDeg(-20)
         self.assertEqual(a.abs(), 20)
 
     def test_radian(self):
-        self.fail()
+        a = AngleDeg(30)
+        self.assertAlmostEquals(a.radian(), 0.523599 , 6)
 
     def test_reverse(self):
         a = AngleDeg(-20)
-        self.assertEqual(a.reverse().degree(), 160)
+        a.reverse()
+        self.assertEqual(a.degree(), 160)
         self.assertEqual(a.degree(), 160)
 
     def test_reverse_angle(self):
@@ -58,11 +55,40 @@ class TestAngleDeg(TestCase):
         self.assertNotEqual(a.degree(), 160)
 
     def test_is_left_of(self):
-        self.fail()
+        a = AngleDeg(20)
+        b = AngleDeg(30)
+        c = AngleDeg(30)
+        d = AngleDeg(45)
+        self.assertFalse(c.is_left_of(b))
+        self.assertTrue(c.is_left_of(d))
+        self.assertFalse(c.is_left_of(a))
 
     def test_is_right_of(self):
-        self.fail()
+        a = AngleDeg(20)
+        b = AngleDeg(30)
+        c = AngleDeg(30)
+        d = AngleDeg(45)
+        self.assertFalse(c.is_right_of(b))
+        self.assertFalse(c.is_right_of(d))
+        self.assertTrue(c.is_right_of(a))
+    
+    def test_is_left_equal_of(self):
+        a = AngleDeg(20)
+        b = AngleDeg(30)
+        c = AngleDeg(30)
+        d = AngleDeg(45)
+        self.assertTrue(c.is_left_equal_of(b))
+        self.assertTrue(c.is_left_equal_of(d))
+        self.assertFalse(c.is_left_equal_of(a))
 
+    def test_is_right_equal_of(self):
+        a = AngleDeg(20)
+        b = AngleDeg(30)
+        c = AngleDeg(30)
+        d = AngleDeg(45)
+        self.assertTrue(c.is_right_equal_of(b))
+        self.assertFalse(c.is_right_equal_of(d))
+        self.assertTrue(c.is_right_equal_of(a))
     def test_copy(self):
         a = AngleDeg(10)
         b = a.copy()
@@ -74,40 +100,53 @@ class TestAngleDeg(TestCase):
         self.assertEqual(a.get_normalized(), 0.75)
 
     def test_cos(self):
-        self.fail()
+        a = AngleDeg(-30)
+        b = AngleDeg(30)
+        self.assertAlmostEquals(a.cos(), 0.8660254038)
+        self.assertAlmostEquals(b.cos(), 0.8660254038)
 
     def test_sin(self):
-        self.fail()
+        a = AngleDeg(-30)
+        b = AngleDeg(30)
+        self.assertAlmostEquals(a.sin(), -0.5)
+        self.assertAlmostEquals(b.sin(), 0.5)
+
 
     def test_tan(self):
-        self.fail()
+        a = AngleDeg(-30)
+        b = AngleDeg(30)
+        self.assertAlmostEquals(a.tan(), -0.5773502692)
+        self.assertAlmostEquals(b.tan(),0.5773502692)
 
     def test_rad2deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.rad2deg(0.52359877), 30 , 5)
 
     def test_deg2rad(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.deg2rad(30), 0.523599 , 6)
 
     def test_cos_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.cos_deg(30), 0.8660254038)
 
     def test_sin_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.sin_deg(30), 0.5)
 
     def test_tan_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.tan_deg(30), 0.5773502692)
 
     def test_acos_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.acos_deg(0.8660254038), 30)
 
     def test_asin_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.asin_deg(0.5), 30)
 
     def test_atan_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.atan_deg(0.5773502692), 30)
 
     def test_atan2_deg(self):
-        self.fail()
+        self.assertAlmostEquals(AngleDeg.atan2_deg(10,10), 45)
 
     def test_bisect(self):
-        self.fail()
+        a = AngleDeg(30)
+        b = AngleDeg(0)
+        c = AngleDeg(15)
+        self.assertEqual(AngleDeg.bisect(a,b), c)
