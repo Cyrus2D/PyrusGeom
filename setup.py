8ac35e1931d81d0018db798958e60d8b3c1921b8
mroa4 <omidamini360@gmail.com>
2022-02-06 23:10:37 +0330

    update license and setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
-from setuptools import setup
+"""PyrusGeom Setup file
+"""
+import setuptools
 
-setup(
-   name='PyrusGeom',
-   version='0.0.1',
-   author='Cyrus 2D Team',
-   author_email='nader.zare88@gmail.com',
-   packages=['PyrusGeom', 'PyrusGeomTest'],
-   # scripts=['bin/script1','bin/script2'],
-   # url='http://pypi.python.org/pypi/PackageName/',
-   license='LICENSE',
-   description='An awesome package for geometry',
-   # long_description=open('README.txt').read(),
-   install_requires=[
-   ],
-   python_requires=">=3.6",
-   test_suite='PyrusGeomTest',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyrusGeom',
+    version='0.0.2',
+    author='Cyrus 2D Team',
+    author_email='nader.zare88@gmail.com',
+
+    description='A package for handling geometry and math in SS2D',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/Cyrus2D/PyrusGeom',
+    project_urls={
+        "Issues" : "https://github.com/Cyrus2D/PyrusGeom/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='LICENSE',
+
+    package_dir={"": "PyrusGeom"},
+    packages=setuptools.find_packages(where="PyrusGeom"),
+
+    python_requires=">=3.6",
+    test_suite='PyrusGeomTest',
 )
